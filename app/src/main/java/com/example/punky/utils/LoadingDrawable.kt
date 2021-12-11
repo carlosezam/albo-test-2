8c4d3b56e19f3063f63defe5e9ff7348f19e5d8f
CarlosEzam <carlos.ezam@gmail.com>
2021-12-11 08:06:48 -0600

    Fix setColorFilter warning

--- app/src/main/java/com/example/punky/utils/LoadingDrawable.kt
+++ app/src/main/java/com/example/punky/utils/LoadingDrawable.kt
@@ -3,15 +3,18 @@ package com.example.punky.utils
 import android.content.Context
 import android.graphics.PorterDuff
 import androidx.annotation.ColorRes
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 
 class LoadingDrawable(context: Context, @ColorRes colorId: Int) : CircularProgressDrawable(context) {
     init {
         strokeWidth = 5f
         centerRadius = 12f
-        setColorFilter(
+        colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(
             context.resources.getColor(colorId),
-            PorterDuff.Mode.SRC_IN
+            BlendModeCompat.SRC_IN
         )
         start()
     }
