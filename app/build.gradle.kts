0c063145a9f09a210306d9ff1284069cb361146b
escobarca <escobarca@globalhitss.com>
2024-09-01 19:16:16 -0600

    Se añade modulo para nueva api RickAndMorty

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,8 @@ android {
 
 dependencies {
 
+    implementation(project(":RickAndMorty"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ pluginManagement {
 
 rootProject.name = "Punky"
 include(":app")
+include(":RickAndMorty")
