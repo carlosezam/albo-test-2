da71e24874115d2f6773265a274ca3c233199359
escobarca <escobarca@globalhitss.com>
2024-09-01 19:34:49 -0600

    Se habilita compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.play.publisher) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 allprojects {
