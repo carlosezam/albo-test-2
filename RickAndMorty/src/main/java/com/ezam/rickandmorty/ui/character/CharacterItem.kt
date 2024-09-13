ecfd894c45e1caf486ff08da933d758a685031b9
escobarca <escobarca@globalhitss.com>
2024-09-13 10:15:01 -0600

    chore(dependencias): actualizar BOM de Compose y ajustar rememberRipple

--- RickAndMorty/src/main/java/com/ezam/rickandmorty/ui/character/CharacterItem.kt
+++ RickAndMorty/src/main/java/com/ezam/rickandmorty/ui/character/CharacterItem.kt
@@ -13,12 +13,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -78,7 +78,7 @@ fun CharacterItem(
         modifier = Modifier
             .clickable(
                 interactionSource = remember { MutableInteractionSource() },
-                indication = rememberRipple(),
+                indication = ripple(),
                 onClick = onClick
             ),
         contentAlignment = Alignment.TopCenter
